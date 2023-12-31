12b6a3c60683117e546d2c66eb33c80007598a01
11291996 <paneah@yonsei-ai-login3.localdomain>
2023-12-31 17:15:33 +0900

    12312023

--- test.cpp
+++ test.cpp
@@ -1,5 +1,18 @@
-long long int end = 100000000;
+#include <iostream>
+#include <utility>
+#include <atomic>
+
+using namespace std;
+
+struct A { int a[100]; };
+    atomic<A> myA;
+    atomic<int> i;
+
+//needs -latomic in compiling command
 
 int main() {
-    for (int i = 0; i < end; i ++) {};
+    cout << boolalpha
+        << myA.is_lock_free() << ' ' // likely false
+        << i.is_lock_free() << '\n'; // likely true
+    return 0;
 }
